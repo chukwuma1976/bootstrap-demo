cbfa37e7345a91ab5f2eac013325f2dc21d19317
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2024-08-20 23:01:48 -0500

    added buttons component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { ContainerComponent } from './container/container.component';
 import { HomeComponent } from './home/home.component';
+import { ButtonsComponent } from './buttons/buttons.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
-    { path: 'container', component: ContainerComponent }
+    { path: 'container', component: ContainerComponent },
+    { path: 'buttons', component: ButtonsComponent }
 ];
