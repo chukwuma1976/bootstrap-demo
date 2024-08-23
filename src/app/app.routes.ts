133f56a49c4a332fb04ddc716d31561e12810750
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2024-08-22 19:55:09 -0500

    added to typography component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,14 @@ import { Routes } from '@angular/router';
 import { ContainerComponent } from './container/container.component';
 import { HomeComponent } from './home/home.component';
 import { ButtonsComponent } from './buttons/buttons.component';
+import { CardComponent } from './card/card.component';
+import { TypographyComponent } from './typography/typography.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
     { path: 'container', component: ContainerComponent },
-    { path: 'buttons', component: ButtonsComponent }
+    { path: 'buttons', component: ButtonsComponent },
+    { path: 'card', component: CardComponent },
+    { path: 'typography', component: TypographyComponent }
 ];
