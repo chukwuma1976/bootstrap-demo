ac45f1132f608ea2fb337fa446bf3573a739f994
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2024-08-23 17:26:44 -0500

    added images component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { ButtonsComponent } from './buttons/buttons.component';
 import { CardComponent } from './card/card.component';
 import { TypographyComponent } from './typography/typography.component';
+import { ImagesComponent } from './images/images.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
@@ -11,5 +12,6 @@ export const routes: Routes = [
     { path: 'container', component: ContainerComponent },
     { path: 'buttons', component: ButtonsComponent },
     { path: 'card', component: CardComponent },
-    { path: 'typography', component: TypographyComponent }
+    { path: 'typography', component: TypographyComponent },
+    { path: 'images', component: ImagesComponent }
 ];
